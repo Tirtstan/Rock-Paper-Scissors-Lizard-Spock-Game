9a579219634628b641c8ea7dd045e603f6d4250a
Luke Barrows <luke.j.barrows@gmail.com>
2023-05-20 17:42:00 +0200

    complete version

--- Rock, Paper, Scissors, Lizard, Spock/Form1.Designer.cs
+++ Rock, Paper, Scissors, Lizard, Spock/Form1.Designer.cs
@@ -44,9 +44,9 @@
             lblComputerChoice = new Label();
             lblGame1 = new Label();
             lblGame2 = new Label();
-            lblGame4 = new Label();
             lblGame3 = new Label();
             lblWinner = new Label();
+            lblRuling = new Label();
             ((System.ComponentModel.ISupportInitialize)picScissors).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picLizard).BeginInit();
             ((System.ComponentModel.ISupportInitialize)picPaper).BeginInit();
@@ -56,56 +56,61 @@
             // 
             // picScissors
             // 
-            picScissors.Location = new Point(306, 9);
-            picScissors.Margin = new Padding(3, 2, 3, 2);
+            picScissors.Image = Properties.Resources.CamScanner_05_17_2023_11_13_3;
+            picScissors.Location = new Point(350, 12);
             picScissors.Name = "picScissors";
-            picScissors.Size = new Size(105, 90);
+            picScissors.Size = new Size(120, 120);
+            picScissors.SizeMode = PictureBoxSizeMode.StretchImage;
             picScissors.TabIndex = 0;
             picScissors.TabStop = false;
             // 
             // picLizard
             // 
-            picLizard.Location = new Point(416, 9);
-            picLizard.Margin = new Padding(3, 2, 3, 2);
+            picLizard.Cursor = Cursors.No;
+            picLizard.Image = Properties.Resources.CamScanner_05_17_2023_11_13_4;
+            picLizard.Location = new Point(475, 12);
             picLizard.Name = "picLizard";
-            picLizard.Size = new Size(105, 90);
+            picLizard.Size = new Size(120, 120);
+            picLizard.SizeMode = PictureBoxSizeMode.StretchImage;
             picLizard.TabIndex = 1;
             picLizard.TabStop = false;
             // 
             // picPaper
             // 
-            picPaper.Location = new Point(196, 9);
-            picPaper.Margin = new Padding(3, 2, 3, 2);
+            picPaper.Image = Properties.Resources.CamScanner_05_17_2023_11_13_2;
+            picPaper.Location = new Point(224, 12);
             picPaper.Name = "picPaper";
-            picPaper.Size = new Size(105, 90);
+            picPaper.Size = new Size(120, 120);
+            picPaper.SizeMode = PictureBoxSizeMode.StretchImage;
             picPaper.TabIndex = 2;
             picPaper.TabStop = false;
             // 
             // picRock
             // 
-            picRock.Location = new Point(86, 9);
-            picRock.Margin = new Padding(3, 2, 3, 2);
+            picRock.Image = Properties.Resources.CamScanner_05_17_2023_11_13_1;
+            picRock.Location = new Point(98, 12);
             picRock.Name = "picRock";
-            picRock.Size = new Size(105, 90);
+            picRock.Size = new Size(120, 120);
+            picRock.SizeMode = PictureBoxSizeMode.StretchImage;
             picRock.TabIndex = 3;
             picRock.TabStop = false;
             // 
             // picSpock
             // 
-            picSpock.Location = new Point(527, 9);
-            picSpock.Margin = new Padding(3, 2, 3, 2);
+            picSpock.Image = Properties.Resources.CamScanner_05_17_2023_11_13_5;
+            picSpock.Location = new Point(602, 12);
             picSpock.Name = "picSpock";
-            picSpock.Size = new Size(105, 90);
+            picSpock.Size = new Size(120, 120);
+            picSpock.SizeMode = PictureBoxSizeMode.StretchImage;
             picSpock.TabIndex = 4;
             picSpock.TabStop = false;
             // 
             // btnRock
             // 
-            btnRock.Font = new Font("Roboto", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnRock.Location = new Point(86, 104);
-            btnRock.Margin = new Padding(3, 2, 3, 2);
+            btnRock.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRock.Location = new Point(98, 139);
             btnRock.Name = "btnRock";
-            btnRock.Size = new Size(105, 30);
+            btnRock.Size = new Size(120, 40);
             btnRock.TabIndex = 6;
             btnRock.Text = "ROCK";
             btnRock.UseVisualStyleBackColor = true;
@@ -113,11 +118,10 @@
             // 
             // btnPaper
             // 
-            btnPaper.Font = new Font("Roboto", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnPaper.Location = new Point(196, 104);
-            btnPaper.Margin = new Padding(3, 2, 3, 2);
+            btnPaper.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPaper.Location = new Point(224, 139);
             btnPaper.Name = "btnPaper";
-            btnPaper.Size = new Size(105, 30);
+            btnPaper.Size = new Size(120, 40);
             btnPaper.TabIndex = 7;
             btnPaper.Text = "PAPER";
             btnPaper.UseVisualStyleBackColor = true;
@@ -125,11 +129,10 @@
             // 
             // btnScissors
             // 
-            btnScissors.Font = new Font("Roboto", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnScissors.Location = new Point(306, 104);
-            btnScissors.Margin = new Padding(3, 2, 3, 2);
+            btnScissors.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnScissors.Location = new Point(350, 139);
             btnScissors.Name = "btnScissors";
-            btnScissors.Size = new Size(105, 30);
+            btnScissors.Size = new Size(120, 40);
             btnScissors.TabIndex = 8;
             btnScissors.Text = "SCISSORS";
             btnScissors.UseVisualStyleBackColor = true;
@@ -137,11 +140,10 @@
             // 
             // btnLizard
             // 
-            btnLizard.Font = new Font("Roboto", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnLizard.Location = new Point(416, 104);
-            btnLizard.Margin = new Padding(3, 2, 3, 2);
+            btnLizard.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLizard.Location = new Point(475, 139);
             btnLizard.Name = "btnLizard";
-            btnLizard.Size = new Size(105, 30);
+            btnLizard.Size = new Size(120, 40);
             btnLizard.TabIndex = 9;
             btnLizard.Text = "LIZARD";
             btnLizard.UseVisualStyleBackColor = true;
@@ -149,11 +151,10 @@
             // 
             // btnSpock
             // 
-            btnSpock.Font = new Font("Roboto", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            btnSpock.Location = new Point(527, 104);
-            btnSpock.Margin = new Padding(3, 2, 3, 2);
+            btnSpock.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSpock.Location = new Point(602, 139);
             btnSpock.Name = "btnSpock";
-            btnSpock.Size = new Size(105, 30);
+            btnSpock.Size = new Size(120, 40);
             btnSpock.TabIndex = 10;
             btnSpock.Text = "SPOCK";
             btnSpock.UseVisualStyleBackColor = true;
@@ -161,11 +162,10 @@
             // 
             // btnExit
             // 
-            btnExit.Font = new Font("Roboto", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            btnExit.Location = new Point(584, 272);
-            btnExit.Margin = new Padding(3, 2, 3, 2);
+            btnExit.Font = new Font("Microsoft Sans Serif", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            btnExit.Location = new Point(666, 363);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(105, 45);
+            btnExit.Size = new Size(121, 60);
             btnExit.TabIndex = 12;
             btnExit.Text = "EXIT";
             btnExit.UseVisualStyleBackColor = true;
@@ -173,11 +173,10 @@
             // 
             // btnPlayAgain
             // 
-            btnPlayAgain.Font = new Font("Roboto", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            btnPlayAgain.Location = new Point(388, 272);
-            btnPlayAgain.Margin = new Padding(3, 2, 3, 2);
+            btnPlayAgain.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            btnPlayAgain.Location = new Point(443, 363);
             btnPlayAgain.Name = "btnPlayAgain";
-            btnPlayAgain.Size = new Size(190, 45);
+            btnPlayAgain.Size = new Size(217, 60);
             btnPlayAgain.TabIndex = 13;
             btnPlayAgain.Text = "PLAY AGAIN";
             btnPlayAgain.UseVisualStyleBackColor = true;
@@ -186,81 +185,81 @@
             // lblPlayerChoice
             // 
             lblPlayerChoice.AutoSize = true;
-            lblPlayerChoice.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            lblPlayerChoice.Location = new Point(355, 154);
+            lblPlayerChoice.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            lblPlayerChoice.Location = new Point(406, 205);
             lblPlayerChoice.Name = "lblPlayerChoice";
-            lblPlayerChoice.Size = new Size(176, 23);
+            lblPlayerChoice.Size = new Size(245, 29);
             lblPlayerChoice.TabIndex = 14;
             lblPlayerChoice.Text = "PLAYER SELECTED:";
             // 
             // lblComputerChoice
             // 
             lblComputerChoice.AutoSize = true;
-            lblComputerChoice.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            lblComputerChoice.Location = new Point(355, 181);
+            lblComputerChoice.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            lblComputerChoice.Location = new Point(406, 241);
             lblComputerChoice.Name = "lblComputerChoice";
-            lblComputerChoice.Size = new Size(208, 23);
+            lblComputerChoice.Size = new Size(290, 29);
             lblComputerChoice.TabIndex = 15;
             lblComputerChoice.Text = "COMPUTER SELECTED:";
             // 
             // lblGame1
             // 
             lblGame1.AutoSize = true;
-            lblGame1.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            lblGame1.Location = new Point(10, 155);
+            lblGame1.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            lblGame1.Location = new Point(11, 207);
             lblGame1.Name = "lblGame1";
-            lblGame1.Size = new Size(166, 23);
+            lblGame1.Size = new Size(207, 29);
             lblGame1.TabIndex = 16;
             lblGame1.Text = "Winner of game 1:";
             // 
             // lblGame2
             // 
             lblGame2.AutoSize = true;
-            lblGame2.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            lblGame2.Location = new Point(10, 180);
+            lblGame2.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            lblGame2.Location = new Point(11, 240);
             lblGame2.Name = "lblGame2";
-            lblGame2.Size = new Size(166, 23);
+            lblGame2.Size = new Size(207, 29);
             lblGame2.TabIndex = 17;
             lblGame2.Text = "Winner of game 2:";
-            // 
-            // lblGame4
-            // 
-            lblGame4.AutoSize = true;
-            lblGame4.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            lblGame4.Location = new Point(10, 230);
-            lblGame4.Name = "lblGame4";
-            lblGame4.Size = new Size(166, 23);
-            lblGame4.TabIndex = 18;
-            lblGame4.Text = "Winner of game 4:";
             // 
             // lblGame3
             // 
             lblGame3.AutoSize = true;
-            lblGame3.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            lblGame3.Location = new Point(10, 205);
+            lblGame3.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            lblGame3.Location = new Point(11, 273);
             lblGame3.Name = "lblGame3";
-            lblGame3.Size = new Size(166, 23);
+            lblGame3.Size = new Size(207, 29);
             lblGame3.TabIndex = 19;
             lblGame3.Text = "Winner of game 3:";
             // 
             // lblWinner
             // 
             lblWinner.AutoSize = true;
-            lblWinner.Font = new Font("Roboto", 16F, FontStyle.Underline, GraphicsUnit.Point);
-            lblWinner.Location = new Point(10, 284);
+            lblWinner.Font = new Font("Microsoft Sans Serif", 14F, FontStyle.Underline, GraphicsUnit.Point);
+            lblWinner.Location = new Point(11, 382);
             lblWinner.Name = "lblWinner";
-            lblWinner.Size = new Size(197, 27);
+            lblWinner.Size = new Size(230, 29);
             lblWinner.TabIndex = 20;
             lblWinner.Text = "OVERALL WINNER:";
             // 
+            // lblRuling
+            // 
+            lblRuling.AutoSize = true;
+            lblRuling.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblRuling.Location = new Point(406, 307);
+            lblRuling.Name = "lblRuling";
+            lblRuling.Size = new Size(90, 25);
+            lblRuling.TabIndex = 21;
+            lblRuling.Text = "RULING";
+            // 
             // formMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 338);
+            ClientSize = new Size(800, 451);
+            Controls.Add(lblRuling);
             Controls.Add(lblWinner);
             Controls.Add(lblGame3);
-            Controls.Add(lblGame4);
             Controls.Add(lblGame2);
             Controls.Add(lblGame1);
             Controls.Add(lblComputerChoice);
@@ -277,9 +276,10 @@
             Controls.Add(picPaper);
             Controls.Add(picLizard);
             Controls.Add(picScissors);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "formMain";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Rock, Paper, Scissors, Lizard, Spock Game";
+            Activated += formMain_Activated;
             ((System.ComponentModel.ISupportInitialize)picScissors).EndInit();
             ((System.ComponentModel.ISupportInitialize)picLizard).EndInit();
             ((System.ComponentModel.ISupportInitialize)picPaper).EndInit();
@@ -307,8 +307,8 @@
         private Label lblComputerChoice;
         private Label lblGame1;
         private Label lblGame2;
-        private Label lblGame4;
         private Label lblGame3;
         private Label lblWinner;
+        private Label lblRuling;
     }
 }
--- Rock, Paper, Scissors, Lizard, Spock/Form1.cs
+++ Rock, Paper, Scissors, Lizard, Spock/Form1.cs
@@ -3,6 +3,7 @@ namespace Rock__Paper__Scissors__Lizard__Spock
     public partial class formMain : Form
     {
         private string[] choices = new string[5] { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
+        string gameWinner;
 
         // layed out like a table :)
         // double check I got the outcome right
@@ -14,6 +15,9 @@ namespace Rock__Paper__Scissors__Lizard__Spock
             { "Rock", "Lizard", "Scissors", "TIE", "Lizard" },
             { "Spock", "Paper", "Spock", "Lizard", "TIE" },
         };
+        private int gameRound = 0;
+        private int playerScore = 0;
+        private int computerScore = 0;
 
         public formMain()
         {
@@ -25,25 +29,184 @@ namespace Rock__Paper__Scissors__Lizard__Spock
             Application.Exit();
         }
 
-        private void btnPlayAgain_Click(object sender, EventArgs e) { }
-
         private void StartGame(int playerChoice)
         {
             Random random = new();
             int computerChoice = random.Next(0, 5); // returns (0 - 4)
 
             string outcome = GetOutcome(playerChoice, computerChoice);
+
+            if (outcome == "TIE")
+            {
+                gameWinner = "";
+            }
+
+            lblPlayerChoice.Text = "PLAYER SELECTED: " + choices[playerChoice];
+            lblComputerChoice.Text = "COMPUTER SELECTED: " + choices[computerChoice];
+
+            if (outcome == "TIE")
+                return;
+
+            switch (gameRound) // adds outcome to relevant label and reveals them
+            {
+                default:
+                case 1:
+                    lblGame1.Visible = true;
+                    lblGame1.Text += gameWinner;
+                    break;
+                case 2:
+                    lblGame2.Visible = true;
+                    lblGame2.Text += gameWinner;
+                    break;
+                case 3:
+                    lblGame3.Visible = true;
+                    lblGame3.Text += gameWinner;
+                    break;
+            }
+
+            if (gameRound >= 3 || playerScore == 2 || computerScore == 2)
+            {
+                string overallWinner = GetOverallWinner(playerScore, computerScore);
+
+                lblWinner.Text = "OVERALL WINNER: " + overallWinner;
+
+                EndGame();
+            }
+        }
+
+        private string GetOverallWinner(int scorePlayer, int scoreComputer)
+        {
+            string overallWinner;
+
+            if (playerScore > computerScore)
+            {
+                overallWinner = "Player Wins";
+            }
+            else if (playerScore < computerScore)
+            {
+                overallWinner = "Computer Wins";
+            }
+            else
+            {
+                overallWinner = "TIE";
+            }
+
+            return overallWinner;
+        }
+        private void EndGame() // resets the game counter, player and computer score, and disables game buttons (to prevent errors)
+        {
+            gameRound = 0;
+            playerScore = 0;
+            computerScore = 0;
+
+            btnRock.Enabled = false;
+            btnPaper.Enabled = false;
+            btnScissors.Enabled = false;
+            btnLizard.Enabled = false;
+            btnSpock.Enabled = false;
+            lblWinner.Visible = true;
+        }
+
+        private string GetRuling(string playerSelected, string computerSelected)
+        {
+            string ruling = "";
+
+            if (playerSelected == "Rock")
+            {
+                if (computerSelected == "Scissors" || computerSelected == "Lizard")
+                    ruling = "Rock crushes " + computerSelected;
+                else if (computerSelected == "Paper")
+                    ruling = "Paper covers Rock";
+                else if (computerSelected == "Spock")
+                    ruling = "Spock vaporizes Rock";
+                else
+                    ruling = "TIE";
+            }
+            else if (playerSelected == "Paper")
+            {
+                if (computerSelected == "Rock")
+                    ruling = "Paper covers Rock";
+                else if (computerSelected == "Scissors")
+                    ruling = "Scissors cuts Paper";
+                else if (computerSelected == "Lizard")
+                    ruling = "Lizard eats Paper";
+                else if (computerSelected == "Spock")
+                    ruling = "Paper disproves Spock";
+                else
+                    ruling = "TIE";
+            }
+            else if (playerSelected == "Scissors")
+            {
+                if (computerSelected == "Paper")
+                    ruling = "Scissors cuts Paper";
+                else if (computerSelected == "Rock")
+                    ruling = "Rock crushes Scissors";
+                else if (computerSelected == "Lizard")
+                    ruling = "Scissors decapitates Lizard";
+                else if (computerSelected == "Spock")
+                    ruling = "Spock smashes Scissors";
+                else
+                    ruling = "TIE";
+            }
+            else if (playerSelected == "Lizard")
+            {
+                if (computerSelected == "Paper")
+                    ruling = "Lizard eats Paper";
+                else if (computerSelected == "Rock")
+                    ruling = "Rock crushes Lizard";
+                else if (computerSelected == "Scissors")
+                    ruling = "Scissors decapitates Lizard";
+                else if (computerSelected == "Spock")
+                    ruling = "Lizard poisons Spock";
+                else
+                    ruling = "TIE";
+            }
+            else if (playerSelected == "Spock")
+            {
+                if (computerSelected == "Rock")
+                    ruling = "Spock vaporizes Rock";
+                else if (computerSelected == "Paper")
+                    ruling = "Paper disproves Spock";
+                else if (computerSelected == "Scissors")
+                    ruling = "Spock smashes Scissors";
+                else if (computerSelected == "Lizard")
+                    ruling = "Lizard poisons Spock";
+                else
+                    ruling = "TIE";
+            }
+
+            return ruling;
         }
 
         private string GetOutcome(int playerChoice, int computerChoice)
         {
-            // use outcomeArray array and this method's parameters to get the outcome in string
-            // (return "") is just placeholder so it doesn't error
-            // return the string (from the outcomeArray array)
-            // feel free to let me do it if you can't figure it out and I'll explain it on Monday or any other time
-            // use this to see examples and learn: (https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/multidimensional-arrays) ignore 3D arrays obviously, that is too much
+            string playerSelected = choices[playerChoice];
+            string computerSelected = choices[computerChoice];
 
-            return "";
+            string outcome = outcomeArray[playerChoice, computerChoice];
+
+            // won't increment round if tie or add points
+            if (outcome == choices[playerChoice])
+            {
+                playerScore++;
+                gameRound++;
+
+                gameWinner = "Player";
+            }
+            else if (outcome == choices[computerChoice])
+            {
+                computerScore++;
+                gameRound++;
+
+                gameWinner = "Computer";
+            }
+
+            string ruling = GetRuling(playerSelected, computerSelected);
+
+            lblRuling.Text = ruling;
+            lblRuling.Visible = true;
+
+            return outcome;
         }
 
         private void btnRock_Click(object sender, EventArgs e)
@@ -69,6 +232,43 @@ namespace Rock__Paper__Scissors__Lizard__Spock
         private void btnSpock_Click(object sender, EventArgs e)
         {
             StartGame(4); // spock index in choices array is 4
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            lblGame1.Text = "Winner of game 1: ";
+            lblGame1.Visible = false;
+
+            lblGame2.Text = "Winner of game 2: ";
+            lblGame2.Visible = false;
+
+            lblGame3.Text = "Winner of game 3: ";
+            lblGame3.Visible = false;
+
+            lblPlayerChoice.Text = "PLAYER SELECTED: ";
+            lblComputerChoice.Text = "COMPUTER SELECTED: ";
+            lblWinner.Visible = false;
+            lblRuling.Visible = false;
+
+            btnRock.Enabled = true;
+            btnPaper.Enabled = true;
+            btnScissors.Enabled = true;
+            btnLizard.Enabled = true;
+            btnSpock.Enabled = true;
+
+            gameRound = 0;
+            playerScore = 0;
+            computerScore = 0;
+        }
+
+        private void formMain_Activated(object sender, EventArgs e)
+        {
+            ResetGame();
         }
     }
 }
